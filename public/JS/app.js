e28979d0ccb3f450c75c52aa95c4b351b4edfa20
LexeyLex <127655101+LexeyLex@users.noreply.github.com>
2024-01-27 15:45:06 +0300

    Add files via upload

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,40 +1,32 @@
-const links = document.querySelectorAll('.link');
-const filters = document.querySelectorAll('.filter-btn');
 const contactBtn = document.querySelector('.contact-form');
 const firstName = document.querySelector('.first-name');
 const lastName = document.querySelector('.last-name');
 const email = document.querySelector('.email');
 const msg = document.querySelector('.message');
 
-links.forEach(link => {
-    link.addEventListener('click',() =>{
-        links.forEach(ele => ele.classList.remove('active'));
-    links.classList.add('active');
-    })
+const links = document.querySelectorAll('.link');
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-containter');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show');
 })
 
-filters.forEach(filterBtn => {
-    filterBtn.addEventListener("click", () =>{
-        let id = filterBtn.getAttribute('id');
-        let projectCards = document.querySelectorAll('.project-card');
-        projectCards.forEach(card =>{
-            if(card.getAttribute('data-tags').includes(id)){
-                card.classList.remove('hide');
-            }else{
-                card.classList.add('hide');
-            }
-        })
 
-        filters.forEach(btn => btn.classList.remove('active'));
-            filterBtn.classList.add('active');
+links.forEach( link => {
+    link.addEventListener('click',() =>{
+        links.forEach(ele => ele.classList.remove('active'));
+    link.classList.add('active');
     })
 })
 
 contactBtn.addEventListener('click', () =>{
-    if(firstName.value.length && lastName.value.length && email.value.length && msg.value.length){
+    if(firstName.value.length && lastName.value.length && email.value.length && msg.value.length ){
         fetch('/mail', {
             method: 'post',
-            headers: new Headers({'Content-Type': 'application/json'}),
+            headers: new Headers({'Content-Type':'application/json'}),
             body: JSON.stringify({
                 firstname: firstName.value,
                 lastname: lastName.value,
--- public/JS/project.js
+++ public/JS/project.js
@@ -1,13 +1,20 @@
-/*const projectContainer = document.querySelector('.project-container');
+const filters = document.querySelectorAll('.filter-btn');
+const projectContainer = document.querySelector('.project-container');
 
-projects.forEach(project => {
-    projectContainer.innerHTML += `
-    <div class="project-card" data-tags="#all,${project.tags}">
-    <img src="IMG/${project.image}">
-        div class="content">
-            <h1 class="project-name">${project.name}</h1>
-            <span class="tags">${project.tags}</span>
-        </div>
-    </div>
-    `;
-})*/
+
+filters.forEach(filterBtn => {
+    filterBtn.addEventListener("click", () =>{
+        let id = filterBtn.getAttribute('id');
+        let projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach(card =>{
+            if(card.getAttribute('data-tags').includes(id)){
+                card.classList.remove('hide');
+            }else{
+                card.classList.add('hide');
+            }
+        })
+
+        filters.forEach(btn => btn.classList.remove('active'));
+            filterBtn.classList.add('active');
+    })
+})
